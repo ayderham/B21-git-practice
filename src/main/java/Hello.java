c04314b84e50b310c53d72d9695ee1ae7ccb65c7
Ayder <73491734+ayderham@users.noreply.github.com>
2021-01-26 13:50:13 -0500

    B21 learning Git

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,10 +1,9 @@
+
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("B21 learning Git");
-
+        System.out.println("Hello World");
+        System.out.println("B21 future SDET learning Git ");
     }
-
 
 }
